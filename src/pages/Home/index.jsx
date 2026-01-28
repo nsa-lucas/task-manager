8edf26414fad8a730c985474d7b2f0f49b620538
lucas nunes <ns_lucas@hotmail.com>
2026-01-27 21:03:50 -0300

    add page home with import header

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,11 @@
+import Header from '../../components/Header/Index';
+
 import './style.css';
 
 export default function Home() {
     return (
         <div>
+            <Header />
             <h1>Home</h1>
         </div>
     );
