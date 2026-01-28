fdbc611b63762b0f8fca389b98623f59609af3bd
lucas nunes <ns_lucas@hotmail.com>
2026-01-27 21:05:30 -0300

    add import global provider authenticate

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,14 @@
-import {createRoot} from 'react-dom/client';
-import {StrictMode} from 'react';
+import { createRoot } from 'react-dom/client';
+import { StrictMode } from 'react';
 
 import App from './App.jsx';
 import './index.css';
+import { AuthProvider } from './auth/AuthProvider.jsx';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <App />
-    </StrictMode>
+        <AuthProvider>
+            <App />
+        </AuthProvider>
+    </StrictMode>,
 );
