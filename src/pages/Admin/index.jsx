01033a4ecc449d8aeae45107ebe368df9219e465
lucas nunes <ns_lucas@hotmail.com>
2026-01-27 22:00:59 -0300

    list and add tasks in memory for test

--- src/pages/Admin/index.jsx
+++ src/pages/Admin/index.jsx
@@ -1,14 +1,78 @@
-import { onAuthStateChanged } from 'firebase/auth';
 import { useEffect, useState } from 'react';
 
 import { auth } from '../../services/firebase';
+import useAuth from '../../auth/useAuth.js';
 import HeaderAdmin from '../../components/HeaderAdmin/Index';
 
 export default function Admin() {
+    const { user, loading } = useAuth();
+    const [task, setTask] = useState('');
+    const [tasks, setTasks] = useState([
+        {
+            name: 'tarefa 1',
+            status: 'pendente',
+        },
+        {
+            name: 'tarefa 1',
+            status: 'pendente',
+        },
+        {
+            name: 'tarefa 1',
+            status: 'pendente',
+        },
+        {
+            name: 'tarefa 1',
+            status: 'pendente',
+        },
+        {
+            name: 'tarefa 1',
+            status: 'pendente',
+        },
+    ]);
+
     return (
         <div>
             <HeaderAdmin />
             <h1>Minhas tarefas</h1>
+            <div>
+                <div>
+                    <input
+                        type="text"
+                        value={task}
+                        onChange={(e) => {
+                            setTask(e.target.value);
+                        }}
+                    />
+                    <button
+                        onClick={() => {
+                            const newTask = {
+                                name: task,
+                                status: 'pendente',
+                            };
+
+                            setTasks([...tasks, newTask]);
+                            setTask('');
+                        }}
+                    >
+                        Adicionar
+                    </button>
+                </div>
+                {tasks.length === 0 ? (
+                    <h4>Nenhuma tarefa adicionada.</h4>
+                ) : (
+                    tasks.map((task, index) => {
+                        return (
+                            <li key={index}>
+                                <span>{task.name}</span>
+
+                                <span>{task.status}</span>
+
+                                <button>Alterar</button>
+                            </li>
+                        );
+                    })
+                )}
+            </div>
         </div>
     );
 }
