81cf560be60ad5b967847be3936c59cbe4075630
lucas nunes <ns_lucas@hotmail.com>
2026-01-30 08:44:49 -0300

    feature: lib react-toastfy

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,5 +1,11 @@
+import { ToastContainer } from 'react-toastify';
 import RoutesApp from './routes';
 
 export default function App() {
-    return <RoutesApp />;
+    return (
+        <>
+            <ToastContainer autoClose={2500} theme="light" />
+            <RoutesApp />;
+        </>
+    );
 }
